7dcb411812bdc6fdcba8f258bca1174e7ccb6efe
mitchelle <mitchellenjoki7@gmail.com>
2022-06-08 10:47:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    
+    } 
+     
+    function mondayWork (activity = "go to the office") {
+        return `This Monday, I will ${activity}.`
+    }
+    
+     const wrapAdjective = function(style='*') {
+        return function(adjective='hard worker')
+        {
+            return `You are ${style}${adjective}${style}!`
+            // console.log (`You are ${style}${adjective}${style}!`)
+        }
+    }
+    // wrapAdjective("||")
+    
+    
+        
+        
+        // let wrapAdjective = function(style='||'){
+        //     return function (adjective = 'a dedicated programmer'){
+        //         return`You are ${style}${adjective}${style}!`;
+        //     }
+        // }
+    
